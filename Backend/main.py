fd0d7e5ae1e8a592c7366be90c6ef4688b1c7d36
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-08-04 15:58:04 +0200

    DB - Testing
    Successfully added and printed out data.
    Working Version Beta-1.3.3

--- Backend/main.py
+++ Backend/main.py
@@ -1,23 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from .database import Base, engine, SessionLocal
+from database import Base, engine, SessionLocal
 from models import User
 import logging as log
+
+
 
 def log_info(message) -> None:
     print("_" * 30)
     log.info(message)
 
 @asynccontextmanager
-async def lifespan():
+async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     try:
         if not db.query(User).first():
             users = [
-                User(username= "Kalifonix", user_hashed_password= "NotAnyMore"),
-                   User(username= "Honda", user_hashed_password= "1234"),
+                    User(username= "Kalifonix", user_hashed_password= "NotAnyMore"),
+                    User(username= "Honda", user_hashed_password= "1234"),
                ]
             db.add_all(users)
             db.commit()
@@ -37,7 +39,15 @@ class GameBackend:
     def root_site(self) -> None:
         @self.app.get("/")
         async def root():
-            return {"NonE":"NonE"}
+            db = SessionLocal()
+            users = db.query(User).all()
+            db.close()
+            return [
+                {"user_id": user.user_id, "username": user.username}
+                for user in users
+            ]
         
+app = GameBackend().app
+
 if __name__ == "__main__":
-    uvicorn.run(GameBackend().app, port=8000, host="0.0.0.0")
+    uvicorn.run(app, port=8000, host="0.0.0.0")
